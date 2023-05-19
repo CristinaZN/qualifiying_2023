489df657188e98ae220dfbe7f5be229d9ae6df73
ge85gix <zhinan@example.com>
2023-05-19 23:26:31 +0200

    for test

--- solution.h
+++ solution.h
@@ -47,108 +47,110 @@ std::vector<ec::Float> process_signal(const std::vector<ec::Float>& inputSignal)
   std::vector<ec::Float> signalFreqImag(WINDOW_SIZE);
   std::vector<ec::Float> spectrumWindow(sizeSpectrum);
   std::vector<ec::Float> outputSpectrum(sizeSpectrum, std::numeric_limits<float>::lowest());
-
+    ec::VecHw& hwInputSingal= *ec::VecHw::getSingletonVecHw();
+    hwInputSingal.copyToHw(inputSignal,0,inputSignal.size(),0);
   size_t idxStartWin = 0;
+
   size_t step = WINDOW_SIZE;
 
-  // Initilize a vector I = [0 , 1 , 2 , 3, ...., WINDOW_SIZE - 1]
-  std::vector<ec::Float> vecI(WINDOW_SIZE*4,0);
-  for(size_t i = 0 ; i < WINDOW_SIZE; i++){
-    vecI[i] = i;
-  }
-  // Initialize a vector A = [a, a, ..., a]
-  ec::Float constant = 2.0f * PI / (WINDOW_SIZE -1);//这一步应该没必要用vechw？
-  std::vector<ec::Float> vecA = valueVector(constant,WINDOW_SIZE);
 
-  ec::VecHw& hwI = *ec::VecHw::getSingletonVecHw();
-  // hwI = [vecI, vecA, 0, 0]
-  hwI.copyToHw(vecI,0,WINDOW_SIZE,0);
-  hwI.copyToHw(vecA,0,WINDOW_SIZE,WINDOW_SIZE);
-  // compute (ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
-  // hwI = [vecI, result, 0, 0]
-  for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
-    hwI.mul32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
-  }
-  // compute cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
-  // hwI = [vecI, result, 0, 0]
-  for (size_t i = 0; i < WINDOW_SIZE / cos_opt_num; i++){
-    hwI.cos4(WINDOW_SIZE+i*cos_opt_num,WINDOW_SIZE+i*cos_opt_num,cos_opt_num);
-  }
-  // compute - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
-  // hwI = [vector(-0.5), result, 0, 0]
-  constant = -0.5f;
-  vecA = valueVector(constant,WINDOW_SIZE);
-  hwI.copyToHw(vecA,0,WINDOW_SIZE,0);
-  for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
-    hwI.mul32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
-  }
-  // hwI.mul32(0,WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
-  // compute 0.42f - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
-  // hwI = [vector(0.42), result, 0, 0]
-  constant = 0.42f;
-  vecA = valueVector(constant,WINDOW_SIZE);
-  hwI.copyToHw(vecA,0,WINDOW_SIZE,0);
-  for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
-    hwI.acc32(i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
-    // hwI.add32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
-  }
-  // hwI.add32(0,WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
-  // hwI = [vector(0.42), result1, 0, result1]
-  // result1 = 0.42f - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1))
-  for (size_t i = 0 ; i< WINDOW_SIZE / opt_num; i++){
-    hwI.assign32(WINDOW_SIZE+i*opt_num,WINDOW_SIZE*3 + i * opt_num, opt_num);
-  }
-  // hwI.assign32(WINDOW_SIZE,WINDOW_SIZE*3,WINDOW_SIZE);
+    // Initilize a vector I = [0 , 1 , 2 , 3, ...., WINDOW_SIZE - 1]
+    std::vector<ec::Float> vecI(WINDOW_SIZE*4,0);
+    for(size_t i = 0 ; i < WINDOW_SIZE; i++){
+        vecI[i] = i;}
+    // Initialize a vector A = [a, a, ..., a]
+    ec::Float constant = 2.0f * PI / (WINDOW_SIZE -1);//这一步应该没必要用vechw？
+    std::vector<ec::Float> vecA = valueVector(constant,WINDOW_SIZE);
+
+    ec::VecHw& hwI = *ec::VecHw::getSingletonVecHw();
+    // hwI = [vecI, vecA, 0, 0]
+    hwI.copyToHw(vecI,0,WINDOW_SIZE,0);
+    hwI.copyToHw(vecA,0,WINDOW_SIZE,WINDOW_SIZE);
+    // compute (ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
+    // hwI = [vecI, result, 0, 0]
+    for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
+        hwI.mul32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
+    }
+    // compute cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
+    // hwI = [vecI, result, 0, 0]
+    for (size_t i = 0; i < WINDOW_SIZE / cos_opt_num; i++){
+        hwI.cos4(WINDOW_SIZE+i*cos_opt_num,WINDOW_SIZE+i*cos_opt_num,cos_opt_num);
+    }
+    // compute - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
+    // hwI = [vector(-0.5), result, 0, 0]
+    constant = -0.5f;
+    vecA = valueVector(constant,WINDOW_SIZE);
+    hwI.copyToHw(vecA,0,WINDOW_SIZE,0);
+    for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
+        hwI.mul32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
+    }
+    // hwI.mul32(0,WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
+    // compute 0.42f - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
+    // hwI = [vector(0.42), result, 0, 0]
+    constant = 0.42f;
+    vecA = valueVector(constant,WINDOW_SIZE);
+    hwI.copyToHw(vecA,0,WINDOW_SIZE,0);
+    for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
+        hwI.add32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
+    }
+    // hwI.add32(0,WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
+    // hwI = [vector(0.42), result1, 0, result1]
+    // result1 = 0.42f - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1))
+    for (size_t i = 0 ; i< WINDOW_SIZE / opt_num; i++){
+        hwI.assign32(WINDOW_SIZE+i*opt_num,WINDOW_SIZE*3 + i * opt_num, opt_num);
+    }
+    // hwI.assign32(WINDOW_SIZE,WINDOW_SIZE*3,WINDOW_SIZE);
 
 
-  constant = 4.0f * PI / (WINDOW_SIZE -1);
-  vecA = valueVector(constant,WINDOW_SIZE);
-  // hwI = [vecI,vecA,0,result1]
-  hwI.copyToHw(vecI,0,WINDOW_SIZE,0);
-  hwI.copyToHw(vecA,0,WINDOW_SIZE,WINDOW_SIZE);
-  //compute (ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
-  // hwI = [vecI,result(vecI.*vecA),0,result1]
-  for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
-    hwI.mul32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
-  }
-  // hwI.mul32(0,WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
-  //compute cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
-  // hwI = [vecI,result(cos(vecI*vecA)),0,result1]
-  for (size_t i = 0; i < WINDOW_SIZE / cos_opt_num; i++){
-    hwI.cos4(WINDOW_SIZE+i*cos_opt_num,WINDOW_SIZE+i*cos_opt_num,cos_opt_num);
-  }
-  // hwI.cos4(WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
-  //compute 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
-  constant = 0.08f;
-  vecA = valueVector(constant,WINDOW_SIZE);
-  hwI.copyToHw(vecA,0,WINDOW_SIZE,0);
-  // hwI = [vector(0.08),result(0.08*cos(vecI*vecA)),0,result1]
-  for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
-    hwI.mul32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
-  }
-  // hwI.mul32(0,WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
-  // compute blackmanWinCoef + 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1));
-  // hwI = [final_result,result(0.08*cos(vecI*vecA)),0,result1]
-  // final_result = result1 + 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1))
-  for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
-    hwI.add32(WINDOW_SIZE + i * opt_num,WINDOW_SIZE * 3 + i * opt_num,i*opt_num,opt_num);
-  }
-  // hwI.add32(WINDOW_SIZE,WINDOW_SIZE*3,0,WINDOW_SIZE);
-  // ec::Float blackmanWinCoef = 0.42f - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1));                                         
-  // blackmanWinCoef = blackmanWinCoef + 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1));
+    constant = 4.0f * PI / (WINDOW_SIZE -1);
+    vecA = valueVector(constant,WINDOW_SIZE);
+    // hwI = [vecI,vecA,0,result1]
+    hwI.copyToHw(vecI,0,WINDOW_SIZE,0);
+    hwI.copyToHw(vecA,0,WINDOW_SIZE,WINDOW_SIZE);
+    //compute (ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
+    // hwI = [vecI,result(vecI.*vecA),0,result1]
+    for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
+        hwI.mul32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
+    }
+    // hwI.mul32(0,WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
+    //compute cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
+    // hwI = [vecI,result(cos(vecI*vecA)),0,result1]
+    for (size_t i = 0; i < WINDOW_SIZE / cos_opt_num; i++){
+        hwI.cos4(WINDOW_SIZE+i*cos_opt_num,WINDOW_SIZE+i*cos_opt_num,cos_opt_num);
+    }
+    // hwI.cos4(WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
+    //compute 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1)), copy to hwI[WinSize+1:2*WinSize]
+    constant = 0.08f;
+    vecA = valueVector(constant,WINDOW_SIZE);
+    hwI.copyToHw(vecA,0,WINDOW_SIZE,0);
+    // hwI = [vector(0.08),result(0.08*cos(vecI*vecA)),0,result1]
+    for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
+        hwI.mul32(i*opt_num,WINDOW_SIZE+i*opt_num,WINDOW_SIZE+i*opt_num,opt_num);
+    }
+    // hwI.mul32(0,WINDOW_SIZE,WINDOW_SIZE,WINDOW_SIZE);
+    // compute blackmanWinCoef + 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1));
+    // hwI = [final_result,result(0.08*cos(vecI*vecA)),0,result1]
+    // final_result = result1 + 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1))
+    for (size_t i = 0; i < WINDOW_SIZE / opt_num; i++){
+        hwI.add32(WINDOW_SIZE + i * opt_num,WINDOW_SIZE * 3 + i * opt_num,i*opt_num,opt_num);
+    }
+    // hwI.add32(WINDOW_SIZE,WINDOW_SIZE*3,0,WINDOW_SIZE);
+    // ec::Float blackmanWinCoef = 0.42f - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1));
+    // blackmanWinCoef = blackmanWinCoef + 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1));
 
 
   for (size_t J = 0; J < numWins; J++)
   {
 
     
-    // for (size_t I = 0; I < WINDOW_SIZE; I++)
-    // {
-      // ec::Float blackmanWinCoef = 0.42f - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1));                                         
-      // blackmanWinCoef = blackmanWinCoef + 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1));
-      // signalWindow[I] = inputSignal[I + idxStartWin] * blackmanWinCoef;
-    // }
-    
+     for (size_t I = 0; I < WINDOW_SIZE; I++)
+     {
+       ec::Float blackmanWinCoef = 0.42f - 0.5f * ec_cos(ec::Float(I) * 2.0f * PI / (WINDOW_SIZE - 1));
+       blackmanWinCoef = blackmanWinCoef + 0.08f * ec_cos(ec::Float(I) * 4.0f * PI / (WINDOW_SIZE - 1));
+       signalWindow[I] = inputSignal[I + idxStartWin] * blackmanWinCoef;
+     }
+
+
     // hwI = [final_result,signalWindow[idxStartWin:idxStartWin+WinSize-1],0,result1]
     hwI.copyToHw(inputSignal,idxStartWin,WINDOW_SIZE,WINDOW_SIZE);
     // hwI = [final_result,signalWindow[idxStartWin:idxStartWin+WinSize-1],result,result1]
@@ -156,7 +158,18 @@ std::vector<ec::Float> process_signal(const std::vector<ec::Float>& inputSignal)
         hwI.mul32(i * opt_num, WINDOW_SIZE + i * opt_num, WINDOW_SIZE * 2 + i * opt_num, opt_num);
     }
     // hwI.mul32(0,WINDOW_SIZE,WINDOW_SIZE*2,WINDOW_SIZE);
-    hwI.copyFromHw(signalWindow,WINDOW_SIZE*2,WINDOW_SIZE,0);
+      std::vector<ec::Float> signalWindow_dan(WINDOW_SIZE);
+    hwI.copyFromHw(signalWindow_dan,WINDOW_SIZE*2,WINDOW_SIZE,0);
+
+//    for(size_t i = 0; i < WINDOW_SIZE; i++){
+//        if(signalWindow_dan[i] - signalWindow[i] > ec::Float(0.000001f)
+//        || signalWindow_dan[i] - signalWindow[i] < ec::Float(0.000001f)) {
+//            std::cout << "signalWindow_dan[i] != signalWindow[i] at: " << i << std::endl;
+//
+//        }
+//    }
+
+
     compute_fourier_transform(signalWindow, signalFreqReal, signalFreqImag);
 
     ec::VecHw &vecHW2 = *ec::VecHw::getSingletonVecHw();
